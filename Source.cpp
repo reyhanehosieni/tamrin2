9531f10ae28fbbbc8d92c9332590ced7dc192961
reyhanehosieni <92735882+reyhanehosieni@users.noreply.github.com>
2021-10-19 11:39:55 +0330

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+using namespace std;
+int main()
+{
+	int a;
+	string exite;
+	int sum = 0;
+	
+	while (exite != "exit")
+	{
+		cout << "adad ra vared konid";
+		cin >> a;
+		sum = sum + a;
+
+	}
+	cout << sum;
+
+	system("pause");
+
+
+}
